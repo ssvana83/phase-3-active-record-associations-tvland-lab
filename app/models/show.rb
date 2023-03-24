adbad94db12af420029f825f4fdb0c469437b9f7
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-03-24 11:00:19 -0400

    DONE

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+  
+  def actors_list
+    self.actors.collect do |actor|
+      actor.full_name
+    end
+  end
+
+ 
   
 end
